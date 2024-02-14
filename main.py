ed6dbcfc68802393fd0227a57e4a4d5ac91026a2
Cephetir <silyichev@gmail.com>
2024-02-14 17:35:57 +0300

    Task 6

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import task1, task2, task3, task4, task5
 
 # Задача 1
-#task1.copy('vacancy_new.csv')
-#task1.print_top_salary()
+task1.copy('vacancy_new.csv')
+task1.print_top_salary()
 
 # Задача 2
-#task2.printBestVacancies()
+task2.printBestVacancies()
 
 # Задача 3
-#task3.findByCompanyName()
+task3.findByCompanyName()
 
 # Задача 4
-#task4.calculatePercent()
+task4.calculatePercent()
 
 # Задача 5
 companies = task5.groupCompanies()
